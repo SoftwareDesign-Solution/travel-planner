bf1d3a674c3c911b95676452047149f004a43fe8
softwaredesign-solution <mail@softwaredesign-solution.de>
2026-09-21 15:29:19 +0200

    chore(nuxt): configure Tailwind CSS

--- apps/travel-planner-nuxt/nuxt.config.ts
+++ apps/travel-planner-nuxt/nuxt.config.ts
@@ -1,25 +1,57 @@
+import { defineNuxtModule } from 'nuxt/kit';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { defineNuxtConfig } from 'nuxt/config';
+import tailwindcss from "@tailwindcss/vite";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const stripRouterVolarPlugin = defineNuxtModule({
+  meta: {
+    name: 'strip-router-volar-plugin',
+  },
+
+  setup(_options, nuxt) {
+    nuxt.hook('modules:done', () => {
+      nuxt.hook('prepare:types', ({ tsConfig }) => {
+        const plugins = tsConfig.vueCompilerOptions?.plugins;
+
+        if (!Array.isArray(plugins)) {
+          return;
+        }
+
+        tsConfig.vueCompilerOptions!.plugins = plugins.filter(
+          (plugin) => plugin !== 'vue-router/volar/sfc-route-blocks',
+        );
+      });
+    });
+  }
+});
+
 export default defineNuxtConfig({
   workspaceDir: '../../',
-  devtools: { enabled: true },
+
+  modules: [stripRouterVolarPlugin],
+
+  devtools: {
+    enabled: true,
+  },
+
   devServer: {
     host: 'localhost',
     port: 4200,
   },
+
   typescript: {
     typeCheck: true,
-    tsConfig: {
-      extends: '../../../tsconfig.base.json', // Nuxt copies this string as-is to the `./.nuxt/tsconfig.json`, therefore it needs to be relative to that directory
-    },
   },
+
   imports: {
     autoImport: true,
   },
+
   css: ['~/assets/css/styles.css'],
+
   vite: {
-    plugins: [nxViteTsPaths()],
+    plugins: [
+      nxViteTsPaths(),
+      tailwindcss()
+    ],
   },
 });
